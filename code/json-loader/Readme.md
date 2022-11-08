e2c8deb9215e1607d1b7d607893c1164af5a71fd
shfshanyue <xianger94@gmail.com>
2022-11-08 09:24:05 +0800

    chore: json

---
+++ code/json-loader/Readme.md
@@ -0,0 +1,13 @@
+# json loader
+
+## 山月的代码实现
+
+代码置于 [shfshanyue/mini-code:code/json-loader](https://github.com/shfshanyue/mini-code/blob/master/code/json-loader/index.js)
+
+可直接读源码，基本每一行都有注释。
+
+使用 `npm run example` 或者 `node example/build.js` 可运行示例代码
+
+``` bash
+$ npm run example
+```
--- code/json-loader/index.js
+++ code/json-loader/index.js
@@ -4,11 +4,9 @@
 // 使用 loader 写一个 json-loader，最能理解 webpack 中 loader 的作用
 
 module.exports = function (source) {
-  let json = typeof source === 'string' ? source : JSON.stringify(source)
-  json = {
-    __user: 'shanyue',
-    ...JSON.parse(json)
-  }
+  // 为了避免 JSON 有语法错误，所以先 parse/stringify 一遍
+  return `module.exports = ${JSON.stringify(JSON.parse(source))}`
 
-  return `module.exports = ${JSON.stringify(json)}`
+  // 写成 ESM 格式也可以，但是 webpack 内部还需要将 esm 转化为 cjs，为了降低复杂度，直接使用 cjs
+  // return `export default ${JSON.stringify(JSON.parse(source))}`
 }
